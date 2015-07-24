70fc0c6c22a77f1f31c9d9b6d1c6b31d5bdb797b
Fawad Khaliq <fawad@plumgrid.com>
2015-07-24 03:29:25 -0700

    Fix py27 job
    Change-Id: Ifb753c3259050c0f172a130077d699b86d7cfdb6
    Signed-off-by: Fawad Khaliq <fawad@plumgrid.com>

--- networking_plumgrid/neutron/tests/unit/test_networking_plumgrid.py
+++ networking_plumgrid/neutron/tests/unit/test_networking_plumgrid.py
@@ -17,7 +17,7 @@ Test cases for  Neutron PLUMgrid Plug-in
 """
 
 import mock
-from oslo.utils import importutils
+from oslo_utils import importutils
 
 from networking_plumgrid.neutron.plugins import plugin as plumgrid_plugin
 from neutron import context
