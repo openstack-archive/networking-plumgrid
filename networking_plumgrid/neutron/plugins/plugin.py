44dd6e7e3830151f947e6f4ee8856397a72fe38b
Shahbaz Nazir <shahbazn@plumgrid.com>
2016-02-11 22:03:32 -0800

    making input parameters case insensitive
    making the input parameters for create network API
    case insensitive  to avoid checks at multiple locations
    
    Closes-Bug: #1544856
    Change-Id: I2fedd6a4a10d1480481b58c7ef7a6b2aa65c6f3d

--- networking_plumgrid/neutron/plugins/plugin.py
+++ networking_plumgrid/neutron/plugins/plugin.py
@@ -20,6 +20,7 @@ import netaddr
 from oslo_config import cfg
 from oslo_log import log as logging
 from oslo_utils import importutils
+from six import string_types
 from sqlalchemy.orm import exc as sa_exc
 
 import networking_plumgrid
@@ -170,6 +171,8 @@ class NeutronPluginPLUMgridV2(agents_db.AgentDbMixin,
 
         LOG.debug('networking-plumgrid: create_network() called')
 
+        # Process network attributes for consistency
+        self._process_network_db(network)
         (network_type, physical_network,
          segmentation_id) = self._process_provider_create(context,
                                                           network['network'])
@@ -1480,3 +1483,10 @@ class NeutronPluginPLUMgridV2(agents_db.AgentDbMixin,
         return super(NeutronPluginPLUMgridV2,
                    self).get_transit_domains(context, filters,
                              fields, sorts, limit, marker, page_reverse)
+
+    def _process_network_db(self, network):
+        if ('provider:network_type' in network['network'] and
+            isinstance(network['network']['provider:network_type'],
+                       string_types)):
+            net_type = network['network']['provider:network_type'].lower()
+            network['network']['provider:network_type'] = net_type
