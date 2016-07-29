6588eba10994b8339a3146efdf48e9e9434f0b23
Muneeb Ahmad <muneeb@plumgrid.com>
2016-07-29 10:57:36 -0700

    Introduce Policy APIs
    * Added the following APIs + CLIs:
    
     ** Policy Tag
     ** Endpoint Group
     ** Endpoint
     ** Policy Service
     ** Policy Service Chain (WIP)
     ** Policy Rule
    
    Co-Authored-By: Muawia Khan <muawia@plumgrid.com>
    Change-Id: Iac394ff6919503c16bcd7a6fdef82b4444ed9f44

--- networking_plumgrid/neutron/plugins/common/pg_helper.py
+++ networking_plumgrid/neutron/plugins/common/pg_helper.py
@@ -13,6 +13,17 @@
 # under the License.
 # service type constants:
 
+from networking_plumgrid.neutron.plugins.common import \
+    policy_exceptions as policy_exc
+from networking_plumgrid.neutron.plugins.common import exceptions as plum_excep
+from networking_plumgrid.neutron.plugins.common import constants as \
+    net_pg_const
+from oslo_log import log as logging
+from oslo_utils import uuidutils
+
+
+LOG = logging.getLogger(__name__)
+
 
 def _retrieve_subnet_dict(self, port_db, context):
     """
@@ -27,3 +38,259 @@ def _retrieve_subnet_dict(self, port_db, context):
                          ["subnet_id"])
             subnet_db[subnet_index] = self._get_subnet(context, subnet_id)
     return subnet_db
+
+
+def _check_floatingip_in_use(self, fp_id, ptag_db):
+    """
+    Helper function to check if Floating IP already exists and
+    is associated with a SecurityGroup/PolicyTag
+    """
+    for entry in ptag_db:
+        if fp_id == entry["floatingip_id"]:
+            return True
+    return False
+
+
+def _check_floatingip_network(self, context, ptag_db):
+    """
+    Helper function to check Floating IP is from the specified
+    External Network
+    """
+    floatingip_db = self.get_floatingip(context,
+                                        ptag_db["policy_tag"]["floatingip_id"])
+    router_db = self.get_routers(context)
+    floating_net_id = floatingip_db["floating_network_id"]
+
+    if floatingip_db.get('port_id') is not None:
+        raise policy_exc.FloatingIPAlreadyInUse(id=str(floatingip_db["id"]))
+
+    external_gateway = []
+    if "router_id" in ptag_db["policy_tag"] and \
+                      ptag_db["policy_tag"]["router_id"] is not None:
+        external_gateway.append(ptag_db["policy_tag"]["router_id"])
+    else:
+        for router in router_db:
+            if "external_gateway_info" in router and \
+             router["external_gateway_info"] and \
+             router["external_gateway_info"]["network_id"] == floating_net_id:
+                external_gateway.append(router["id"])
+        if len(external_gateway) == 0:
+            raise policy_exc.GatewayNotSet(id=str(floatingip_db["id"]))
+        if len(external_gateway) > 1:
+            raise policy_exc.MultipleExternalGatewaysFound(
+                                           gateway_list=external_gateway)
+    return (floatingip_db, external_gateway[0])
+
+
+def _check_duplicate_ports_policy_service_create(self, ps_db):
+    """
+    Helper function to check if duplicate ingress, egress, bidirectional
+    ports exists in create policy service JSON
+    """
+    port_list = []
+    if "ingress_ports" in ps_db and ps_db["ingress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                         in ps_db["ingress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["ingress_ports"]):
+            return True, "ingress ports"
+
+    if "egress_ports" in ps_db and ps_db["egress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                         in ps_db["egress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["egress_ports"]):
+            return True, "egress ports"
+
+    if "bidirectional_ports" in ps_db and ps_db["bidirectional_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                         in ps_db["bidirectional_ports"]]))
+        if _check_duplicate_ports_config(ps_db["bidirectional_ports"]):
+            return True, "bidirectional ports"
+
+    if _check_duplicate_ports_across_config(port_list):
+        return True, "create parameters"
+
+    return False, None
+
+
+def _check_duplicate_ports_policy_service_update(self, ps_db):
+    """
+    Helper function to check if duplicate ingress, egress, bidirectional
+    ports exists in update policy service JSON
+    """
+    port_list = []
+    if "add_ingress_ports" in ps_db and ps_db["add_ingress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                         in ps_db["add_ingress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["add_ingress_ports"]):
+            return True, "add ingress ports"
+
+    if "add_egress_ports" in ps_db and ps_db["add_egress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                          in ps_db["add_egress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["add_egress_ports"]):
+            return True, "add egress ports"
+
+    if "add_bidirectional_ports" in ps_db and ps_db["add_bidirectional_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                          in ps_db["add_bidirectional_ports"]]))
+        if _check_duplicate_ports_config(ps_db["add_bidirectional_ports"]):
+            return True, "add bidirectional ports"
+
+    if "remove_ingress_ports" in ps_db and ps_db["remove_ingress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                          in ps_db["remove_ingress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["remove_ingress_ports"]):
+            return True, "remove ingress ports"
+
+    if "remove_egress_ports" in ps_db and ps_db["remove_egress_ports"]:
+        port_list.append(set([tuple(d.items()) for d
+                          in ps_db["remove_egress_ports"]]))
+        if _check_duplicate_ports_config(ps_db["remove_egress_ports"]):
+            return True, "remove engress ports"
+
+    if ("remove_bidirectional_ports" in ps_db
+        and ps_db["remove_bidirectional_ports"]):
+        port_list.append(set([tuple(d.items()) for d
+                          in ps_db["remove_bidirectional_ports"]]))
+        if _check_duplicate_ports_config(ps_db["remove_bidirectional_ports"]):
+            return True, "remove bidirectional ports"
+
+    if _check_duplicate_ports_across_config(port_list):
+        return True, "update parameters"
+
+    return False, None
+
+
+def _check_duplicate_ports_config(port_db):
+    if (len(port_db) > len([dict(t) for t in
+        set([tuple(d.items()) for d in port_db])])):
+        return True
+
+
+def _check_duplicate_ports_across_config(list_port_db):
+    for i in range(0, len(list_port_db)):
+        for j in range(i+1, len(list_port_db)):
+            if (len(list_port_db[i].intersection(list_port_db[j])) > 0):
+                return True
+
+
+def _validate_port_owner(port_db):
+    """
+    Helper function to validate port is owned by a compute resource
+    """
+    if ("device_owner" in port_db and "binding:vif_type" in port_db and
+        "compute" in port_db["device_owner"] and
+        port_db["binding:vif_type"] == net_pg_const.BINDING_VIF_TYPE_IOVISOR):
+        return True
+    return False
+
+
+def _validate_port_sec_grp_association(port_db):
+    """
+    Helper function to validate if port being added to
+    policy service is not associated with a
+    security group
+    """
+    if "security_groups" in port_db and not port_db["security_groups"]:
+        return True
+    return False
+
+
+def _check_policy_service_leg_mode(ps_obj, updated_ps_obj=None):
+    """
+    Helper function to check if bidirectional ports are
+    specified with ingress or egress ports
+    """
+    if ("ingress_ports" in ps_obj and "egress_ports"
+        in ps_obj and "bidirectional_ports" in ps_obj):
+        if updated_ps_obj is None:
+            if ((ps_obj["ingress_ports"] or ps_obj["egress_ports"])
+                and ps_obj["bidirectional_ports"]):
+                return True
+        else:
+            if ps_obj["ingress_ports"] or ps_obj["egress_ports"]:
+                 if ("add_bidirectional_ports" in updated_ps_obj or
+                     "remove_bidirectional_ports" in updated_ps_obj):
+                     return True
+            elif ps_obj["bidirectional_ports"]:
+                 if ("add_ingress_ports" in updated_ps_obj or
+                     "add_egress_ports" in updated_ps_obj or
+                     "remove_ingress_ports" in updated_ps_obj or
+                     "remove_egress_ports" in updated_ps_obj):
+                     return True
+            else:
+                #Update call with any prior ports in policy service
+                if ((("add_ingress_ports" in updated_ps_obj or
+                      "add_ingress_ports" in updated_ps_obj)
+                     or ("remove_ingress_ports" in updated_ps_obj or
+                         "remove_egress_ports" in updated_ps_obj))
+                    and ("add_bidirectional_ports" in updated_ps_obj or
+                         "remove_bidirectional_ports" in updated_ps_obj)):
+                    return True
+    return False
+
+
+def _is_security_group(context, ep_obj, ep_db, config):
+    if config in ep_db and ep_db[config]:
+        for epg in ep_db[config]:
+            epg_id_list = ep_obj.get_endpoint_groups(context,
+                                     filters={'id': [epg['id']]},
+                                     fields=["id"])
+            if len(epg_id_list) == 1:
+                if ("is_security_group" in epg_id_list[0]
+                    and epg_id_list[0]["is_security_group"]):
+                    operation = "Endpoint association"
+                    raise policy_exc.OperationNotAllowed(operation=operation,
+                                                         id=epg_id_list[0]["id"])
+
+
+def _validate_ep_config(ep_db):
+    if ((ep_db["ip_mask"] and ep_db["ip_port"]) or
+        (ep_db["ip_mask"]and ep_db["port_id"]) or
+        (ep_db["ip_port"] and ep_db["port_id"])):
+        raise policy_exc.MultipleAssociationForEndpoint()
+
+
+def _check_duplicates_endpoint_config(ep_db):
+    if "ep_groups" in ep_db and ep_db["ep_groups"]:
+        if (len(ep_db["ep_groups"]) > len([dict(t) for t in
+            set([tuple(d.items()) for d in ep_db["ep_groups"]])])):
+            raise policy_exc.DuplicateEndpointGroup()
+    if "add_endpoint_groups" in ep_db and ep_db["add_endpoint_groups"]:
+        if (len(ep_db["add_endpoint_groups"]) > len([dict(t) for t in
+            set([tuple(d.items()) for d in ep_db["add_endpoint_groups"]])])):
+            raise policy_exc.DuplicateEndpointGroup()
+    if "remove_endpoint_groups" in ep_db and ep_db["remove_endpoint_groups"]:
+        if (len(ep_db["remove_endpoint_groups"]) > len([dict(t) for t in
+            set([tuple(d.items()) for d in ep_db["remove_endpoint_groups"]])])):
+            raise policy_exc.DuplicateEndpointGroup()
+
+    if (("add_endpoint_groups" in ep_db and ep_db["add_endpoint_groups"]) and
+        ("remove_endpoint_groups" in ep_db and ep_db["remove_endpoint_groups"])):
+        if (len(set([tuple(d.items()) for d in ep_db["add_endpoint_groups"]]).
+                    intersection(set([tuple(d.items()) for d in
+                    ep_db["remove_endpoint_groups"]]))) > 0):
+            raise policy_exc.DuplicateEndpointGroup()
+
+def _process_policy_tag(self, context, epg_db):
+    if "policy_tag_id" in epg_db and epg_db["policy_tag_id"]:
+        if (not uuidutils.is_uuid_like(epg_db['policy_tag_id']) and
+            epg_db['policy_tag_id'] is not None):
+            ptag_list = self.get_policy_tags(context,
+                                 filters={'name': [epg_db['policy_tag_id']]},
+                                 fields=["id"])
+
+            if len(ptag_list) == 1:
+                epg_db['policy_tag_id'] = ptag_list[0]["id"]
+            elif len(ptag_list) == 0:
+                err_message = ("No policy tag"
+                               " matches found for tag"
+                               " '%s'" % epg_db['policy_tag_id'])
+                raise plum_excep.PLUMgridException(err_msg=err_message)
+            else:
+                err_message = ("Multiple policy tag"
+                               " matches found for tag"
+                               " '%s', use an ID to be more"
+                               " specific." % epg_db['policy_tag_id'])
+                raise plum_excep.PLUMgridException(err_msg=err_message)
+    return epg_db
---
+++ networking_plumgrid/neutron/plugins/extensions/endpointgroup.py
@@ -0,0 +1,120 @@
+# Copyright 2016 PLUMgrid, Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import netaddr
+import networking_plumgrid
+from networking_plumgrid._i18n import _
+from neutron.api import extensions
+from neutron.api.v2 import attributes as attr
+from neutron.api.v2 import base
+from neutron.common import exceptions as nexceptions
+from neutron import manager
+
+PG_EPG = 'endpoint_group'
+PG_EPGS = 'endpoint-groups'
+
+extensions.append_api_extensions_path(
+    networking_plumgrid.neutron.plugins.extensions.__path__)
+
+
+class NoEndpointGroupFound(nexceptions.NotFound):
+    message = _("Endpoint Group with id %(id)s does not exist")
+
+class UpdateParametersRequired(nexceptions.InvalidInput):
+    message = _("No update parameter specified atleast one needed")
+
+RESOURCE_ATTRIBUTE_MAP = {
+    'endpoint_group': {
+        'id': {
+            'allow_post': False,
+            'allow_put': False,
+            'validate': {
+                'type:uuid': None
+            },
+            'is_visible': True,
+            'primary_key': True
+        },
+        'name': {
+            'allow_post': True,
+            'allow_put': True,
+            'is_visible': True,
+            'default': ''
+        },
+        'description': {
+            'allow_post': True,
+            'allow_put': True,
+            'is_visible': True,
+            'default': ''
+        },
+        'policy_tag_id': {
+            'allow_post': True,
+            'allow_put': True,
+            'is_visible': True,
+            'default': None,
+        },
+        'is_security_group': {
+            'allow_post': False,
+            'allow_put': False,
+            'is_visible': True,
+            'default': False,
+            'validate': {'type:boolean': None}
+
+        },
+        'tenant_id': {'allow_post': True,
+                      'allow_put': False,
+                      'required_by_policy': True,
+                      'validate': {'type:string': attr.TENANT_ID_MAX_LEN},
+                      'is_visible': True},
+    }
+}
+
+
+class Endpointgroup(extensions.ExtensionDescriptor):
+
+    @classmethod
+    def get_name(cls):
+        return "Endpoint Group"
+
+    @classmethod
+    def get_alias(cls):
+        return "endpoint-group"
+
+    @classmethod
+    def get_description(cls):
+        return "This API will be used to create endpoint groups" \
+               " from Neutron and map it in PLUMgrid."
+
+    @classmethod
+    def get_namespace(cls):
+        return "http://docs.openstack.org/ext/endpoint_group" \
+               "/api/v2.0"
+
+    @classmethod
+    def get_updated(cls):
+        return "2016-01-01T00:00:00-00:00"
+
+    @classmethod
+    def get_resources(cls):
+        exts = []
+        plugin = manager.NeutronManager.get_plugin()
+
+        resource_name = PG_EPG
+        collection_name = PG_EPGS
+        params = RESOURCE_ATTRIBUTE_MAP.get(resource_name, dict())
+        controller = base.create_resource(
+            collection_name, resource_name, plugin, params)
+        ex = extensions.ResourceExtension(collection_name,
+                                          controller)
+        exts.append(ex)
+        return exts
