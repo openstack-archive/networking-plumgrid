6437423d85f0640ab5903b9eaf7aaff673ec1c87
Muneeb Ahmad <muneeb@plumgrid.com>
2016-07-29 10:57:36 -0700

    Introduce Policy APIs
    * Added the following APIs + CLIs:
    
     ** Policy Tag
     ** Endpoint Group
     ** Endpoint
     ** Policy Service
     ** Policy Service Chain (WIP)
     ** Policy Rule
    
    Co-Authored-By: Muawia Khan <muawia@plumgrid.com>
    Change-Id: Iac394ff6919503c16bcd7a6fdef82b4444ed9f44

---
+++ networking_plumgrid/neutron/tests/unit/extensions/test_endpoint_group.py
@@ -0,0 +1,209 @@
+# Copyright 2016 PLUMgrid, Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Endpoint Group extension unit tests
+"""
+from networking_plumgrid.neutron.plugins.common import \
+    exceptions as p_excep
+from networking_plumgrid.neutron.plugins.common import \
+    policy_exceptions as policy_excep
+from networking_plumgrid.neutron.plugins.extensions import \
+    endpointgroup
+from networking_plumgrid.neutron.tests.unit import \
+    test_networking_plumgrid as test_pg
+
+from neutron.api import extensions
+from neutron import context
+from neutron import manager
+from neutron.tests.unit.api import test_extensions as test_ext
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class EndpointGroupExtensionManager(object):
+
+    def get_resources(self):
+        return endpointgroup.Endpointgroup.get_resources()
+
+    def get_actions(self):
+        return []
+
+    def get_request_extensions(self):
+        return []
+
+
+class EndpointGroupTestCase(test_pg.PLUMgridPluginV2TestCase):
+    def setUp(self, plugin=None, ext_mgr=None):
+        super(EndpointGroupTestCase, self).setUp()
+        ext_mgr = EndpointGroupExtensionManager()
+        extensions.PluginAwareExtensionManager._instance = None
+        self.ext_api = test_ext.setup_extensions_middleware(ext_mgr)
+
+
+class TestEndpointGroup(EndpointGroupTestCase):
+    def test_create_endpoint_group(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        epg = self._make_epg_dict()
+
+        epg_ret = plugin.create_endpoint_group(
+                      admin_context, epg)
+        epg["endpoint_group"]["id"] = epg_ret["id"]
+        self.assertEqual(epg_ret, epg["endpoint_group"])
+
+    def test_create_endpoint_group_with_policy_tag(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        ptag = self._fake_policy_tag_dict()
+        ptag_ret = plugin.create_policy_tag(admin_context, ptag)
+        ptag["policy_tag"]["id"] = ptag_ret["id"]
+
+        epg = self._make_epg_dict(ptag_id=ptag_ret["id"])
+
+        epg_ret = plugin.create_endpoint_group(
+                      admin_context, epg)
+        epg["endpoint_group"]["id"] = epg_ret["id"]
+        self.assertEqual(epg_ret, epg["endpoint_group"])
+
+    def test_create_endpoint_group_with_invalid_policy_tag(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        ptag = self._fake_policy_tag_dict()
+        ptag_ret = plugin.create_policy_tag(admin_context, ptag)
+        ptag["policy_tag"]["id"] = ptag_ret["id"]
+
+        epg = self._make_epg_dict(ptag_id="abcdef123")
+
+        self.assertRaises(p_excep.PLUMgridException,
+                          plugin.create_endpoint_group, admin_context, epg)
+
+    def test_create_endpoint_group_with_in_use_policy_tag(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        ptag = self._fake_policy_tag_dict()
+        ptag_ret = plugin.create_policy_tag(admin_context, ptag)
+        ptag["policy_tag"]["id"] = ptag_ret["id"]
+
+        # Create first endpoint group with policy tag
+        epg1 = self._make_epg_dict(ptag_id=ptag_ret["id"])
+        epg_ret1 = plugin.create_endpoint_group(
+                      admin_context, epg1)
+        epg1["endpoint_group"]["id"] = epg_ret1["id"]
+
+        # Create a second endpoint group with same policy tag
+        epg2 = self._make_epg_dict(ptag_id=ptag_ret["id"])
+        self.assertRaises(policy_excep.PolicyTagAlreadyInUse,
+                          plugin.create_endpoint_group, admin_context, epg2)
+
+    def test_create_delete_endpoint_group(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        ptag = self._fake_policy_tag_dict()
+        ptag_ret = plugin.create_policy_tag(admin_context, ptag)
+        ptag["policy_tag"]["id"] = ptag_ret["id"]
+
+        epg = self._make_epg_dict(ptag_id=ptag_ret["id"])
+
+        epg_ret = plugin.create_endpoint_group(
+                      admin_context, epg)
+        epg["endpoint_group"]["id"] = epg_ret["id"]
+
+        resp = plugin.delete_endpoint_group(admin_context, epg_ret["id"])
+        self.assertEqual(None, resp)
+
+    def test_create_update_endpoint_group(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        epg = self._make_epg_dict()
+
+        epg_ret = plugin.create_endpoint_group(
+                      admin_context, epg)
+        epg["endpoint_group"]["id"] = epg_ret["id"]
+
+        epg_update_dict = self._make_epg_update_dict(name="updated-epg-name",
+                                            description="updated-description")
+        epg_update_ret = plugin.update_endpoint_group(admin_context,
+                                                      epg_ret["id"],
+                                                      epg_update_dict)
+        epg_update_dict["endpoint_group"]["id"] = epg_update_ret["id"]
+        self.assertEqual(epg_update_dict["endpoint_group"], epg_update_ret)
+
+    def test_list_all_endpoint_groups(self):
+        plugin = manager.NeutronManager.get_plugin()
+        admin_context = context.get_admin_context()
+
+        # Create first endpoint group
+        epg1 = self._make_epg_dict()
+        epg_ret1 = plugin.create_endpoint_group(
+                      admin_context, epg1)
+        epg1["endpoint_group"]["id"] = epg_ret1["id"]
+
+        # Create second endpoint group
+        ptag = self._fake_policy_tag_dict()
+        ptag_ret = plugin.create_policy_tag(admin_context, ptag)
+        ptag["policy_tag"]["id"] = ptag_ret["id"]
+
+        epg2 = self._make_epg_dict(ptag_id=ptag_ret["id"])
+
+        epg_ret2 = plugin.create_endpoint_group(
+                      admin_context, epg2)
+        epg2["endpoint_group"]["id"] = epg_ret2["id"]
+
+        # List both endpoint groups
+        epg_list_ret = plugin.get_endpoint_groups(admin_context)
+        self.assertItemsEqual(epg_list_ret, [epg_ret1, epg_ret2])
+
+    def _make_epg_dict(self, ptag_id=None,
+                       name="test_sg_name"):
+
+        epg_dict = {"endpoint_group": {
+                   "tenant_id": "test_tenant",
+                   "name": name,
+                   "description": "test_description",
+                   "is_security_group": False}}
+        if ptag_id is not None:
+            epg_dict["endpoint_group"]["policy_tag_id"] = ptag_id
+        return epg_dict
+
+    def _make_epg_update_dict(self, name="test_tenant",
+                              description="test_description"):
+        return {"endpoint_group": {
+                   "tenant_id": "test_tenant",
+                   "name": name,
+                   "description": description,
+                   "is_security_group": False,
+                   "policy_tag_id": None}}
+
+    def _fake_policy_tag_dict(self):
+        return {"policy_tag": {
+                               "tenant_id": "test_tenant",
+                               "name": "test_name",
+                               "tag_type": "dot1q",
+                               "tag_id": "10-20",
+                               "floatingip_id": None,
+                               "floating_ip_address": None,
+                               "router_id": None}}
+
+    def _fake_sg(self):
+        return {"security_group": {"name": "fake-sg",
+                                   "description": "sample-description",
+                                   "tenant_id": "test-tenant"}}
---
+++ networking_plumgrid/neutronclient/policy/endpoint_group.py
@@ -0,0 +1,128 @@
+# Copyright 2016 OpenStack Foundation.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+from networking_plumgrid._i18n import _
+from neutronclient.common import extension
+
+
+class EndpointGroup(extension.NeutronClientExtension):
+    resource = 'endpoint_group'
+    resource_plural = 'endpoint_groups'
+    path = 'endpoint-groups'
+    object_path = '/%s' % path
+    resource_path = '/%s/%%s' % path
+    versions = ['2.0']
+
+
+def args2body(self, parsed_args):
+    try:
+        if parsed_args.name:
+            ep_grp_name = parsed_args.name
+            body = {'endpoint_group': {'name': ep_grp_name}}
+        else:
+            body = {'endpoint_group': {}}
+        if parsed_args.tag:
+            body['endpoint_group']['policy_tag_id'] = parsed_args.tag
+        else:
+            body['endpoint_group']['policy_tag_id'] = None
+        if parsed_args.description:
+            body['endpoint_group']['description'] = parsed_args.description
+        else:
+            body['endpoint_group']['description'] = ''
+        return body
+    except KeyError as err:
+        raise Exception("KeyError: " + str(err))
+
+
+class EndpointGroupCreate(extension.ClientExtensionCreate,
+                          EndpointGroup):
+    """Create a Endpoint Group."""
+
+    shell_command = 'endpoint-group-create'
+
+    def add_known_arguments(self, parser):
+        parser.add_argument(
+            'name', metavar='<ENDPOINT-GROUP-NAME>',
+            help=_('Descriptive name for endpoint group.'))
+        parser.add_argument('--description', dest='description',
+                        help=_('Description of the Endpoint Group '
+                               'being created.'))
+        parser.add_argument('--tag', dest='tag',
+                        help=_('policy-tag name/uuid '))
+        #add_known_arguments(self, parser)
+
+    def args2body(self, parsed_args):
+        body = args2body(self, parsed_args)
+        if parsed_args.tenant_id:
+            (body['endpoint_group']
+                 ['tenant_id']) = parsed_args.tenant_id
+        return body
+
+
+class EndpointGroupList(extension.ClientExtensionList,
+                      EndpointGroup):
+    """List endpoint groups that belong to a given tenant."""
+
+    shell_command = 'endpoint-group-list'
+    list_columns = ['id', 'name', 'policy_tag_id', 'description',
+                    'is_security_group']
+    pagination_support = True
+    sorting_support = True
+
+
+class EndpointGroupShow(extension.ClientExtensionShow,
+                        EndpointGroup):
+    """Show information of a given endpoint group."""
+
+    shell_command = 'endpoint-group-show'
+
+
+class EndpointGroupDelete(extension.ClientExtensionDelete,
+                          EndpointGroup):
+    """Delete a given endpoint group."""
+
+    shell_command = 'endpoint-group-delete'
+
+
+class EndpointGroupUpdate(extension.ClientExtensionUpdate,
+                          EndpointGroup):
+    """Update a given endpoint-group."""
+
+    shell_command = 'endpoint-group-update'
+
+    def add_known_arguments(self, parser):
+        parser.add_argument(
+            '--name', metavar='name',
+            help=_('Descriptive name for Endpoint Group'))
+        parser.add_argument(
+            '--description', metavar='description',
+            help=_('Description for Endpoint Group'))
+
+    def args2body(self, parsed_args):
+        try:
+            if parsed_args.name:
+                ep_name = parsed_args.name
+                body = {'endpoint_group': {'name': ep_name}}
+            else:
+                body = {'endpoint_group': {}}
+                #raise Exception("--name must be specified for updating "
+                #                "Endpoint Group.")
+            if parsed_args.description:
+                (body['endpoint_group'][
+                 'description']) = parsed_args.description
+            return body
+        except KeyError as err:
+            raise Exception("KeyError: " + str(err))
