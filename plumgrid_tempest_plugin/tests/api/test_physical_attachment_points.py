46e4d7467082a6c922fdc353d0e455d5c3b880df
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-03-06 16:23:35 -0800

    Switch to use stable decorators.idempotent_id
    Tempest provides decorators.idempotent_id as stable interface,
    so this patch switches to use it for long-term maintenance.
    
    Change-Id: I85a6c95e77d9697444ff034fe6743ae0c16023de

--- plumgrid_tempest_plugin/tests/api/test_physical_attachment_points.py
+++ plumgrid_tempest_plugin/tests/api/test_physical_attachment_points.py
@@ -12,6 +12,7 @@
 import httplib
 from oslo_log import log as logging
 from tempest import config
+from tempest.lib import decorators
 from tempest import test
 
 from plumgrid_tempest_plugin.tests import base
@@ -43,7 +44,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
         cls.admin_client = cls.os_adm.pap_client
 
     @test.attr(type='smoke')
-    @test.idempotent_id('2fb95a42-482d-45cd-93fd-9e161a709874')
+    @decorators.idempotent_id('2fb95a42-482d-45cd-93fd-9e161a709874')
     def test_create_pap(self):
         resp, pap = self.admin_client.create_pap()
         self.addCleanup(self.admin_client.delete_pap,
@@ -51,7 +52,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
         self.assertEqual(resp.status, httplib.CREATED)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('5d4b2eb6-5ce6-44c3-afb3-b0fb75a15c7c')
+    @decorators.idempotent_id('5d4b2eb6-5ce6-44c3-afb3-b0fb75a15c7c')
     def test_show_pap(self):
         _, pap = self.admin_client.create_pap()
         self.addCleanup(self.admin_client.delete_pap,
@@ -63,7 +64,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
                          body['physical_attachment_point'])
 
     @test.attr(type='smoke')
-    @test.idempotent_id('3ba5892a-5fb1-49fc-b113-1e3b47712a37')
+    @decorators.idempotent_id('3ba5892a-5fb1-49fc-b113-1e3b47712a37')
     def test_update_pap(self):
         resp, pap = self.admin_client.create_pap()
         self.addCleanup(self.admin_client.delete_pap,
@@ -74,7 +75,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
         self.assertEqual(resp.status, httplib.OK)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('3f817924-7ca0-41a3-bf30-ce00079a90d6')
+    @decorators.idempotent_id('3f817924-7ca0-41a3-bf30-ce00079a90d6')
     def test_delete_pap(self):
         _, pap = self.admin_client.create_pap(interfaces=self.interfaces)
         resp, pap = self.admin_client.delete_pap(
@@ -82,7 +83,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
         self.assertEqual(resp.status, httplib.NO_CONTENT)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('fb1e3803-2872-439a-bb4a-e3de50d3ed91')
+    @decorators.idempotent_id('fb1e3803-2872-439a-bb4a-e3de50d3ed91')
     def test_list_paps(self):
         _, pap1 = self.admin_client.create_pap(interfaces=self.interfaces)
         _, pap2 = self.admin_client.create_pap()
@@ -95,7 +96,7 @@ class TestPhysicalAttachmentPoint(base.BaseTest):
         self.assertEqual(len(paps_list['physical_attachment_points']), 3)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('2fb95a42-482d-45cd-93fd-9e161a709874')
+    @decorators.idempotent_id('2fb95a42-482d-45cd-93fd-9e161a709874')
     def test_show_pap_by_name(self):
         _, pap1 = self.admin_client.create_pap()
         _, pap2 = self.admin_client.create_pap()
--- plumgrid_tempest_plugin/tests/api/test_transit_domains.py
+++ plumgrid_tempest_plugin/tests/api/test_transit_domains.py
@@ -11,6 +11,7 @@
 # limitations under the License.
 import httplib
 from oslo_log import log as logging
+from tempest.lib import decorators
 from tempest import test
 
 from plumgrid_tempest_plugin.tests import base
@@ -29,7 +30,7 @@ class TestTransitDomains(base.BaseTest):
         cls.admin_client = cls.os_adm.td_client
 
     @test.attr(type='smoke')
-    @test.idempotent_id('29a30571-2f4c-4bc6-87c4-6f8052e04f93')
+    @decorators.idempotent_id('29a30571-2f4c-4bc6-87c4-6f8052e04f93')
     def test_create_td(self):
         resp, td = self.admin_client.create_td()
         self.addCleanup(self.admin_client.delete_td,
@@ -37,7 +38,7 @@ class TestTransitDomains(base.BaseTest):
         self.assertEqual(resp.status, httplib.CREATED)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('9ff01991-a7df-4a1e-b2e9-5bc0f5dc21fb')
+    @decorators.idempotent_id('9ff01991-a7df-4a1e-b2e9-5bc0f5dc21fb')
     def test_show_td(self):
         _, td = self.admin_client.create_td()
         self.addCleanup(self.admin_client.delete_td,
@@ -49,7 +50,7 @@ class TestTransitDomains(base.BaseTest):
                          body['transit_domain'])
 
     @test.attr(type='smoke')
-    @test.idempotent_id('89b2e4be-1cae-4a88-b6e3-8c100ceebba5')
+    @decorators.idempotent_id('89b2e4be-1cae-4a88-b6e3-8c100ceebba5')
     def test_update_td(self):
         resp, td = self.admin_client.create_td()
         self.addCleanup(self.admin_client.delete_td,
@@ -60,7 +61,7 @@ class TestTransitDomains(base.BaseTest):
         self.assertEqual(resp.status, httplib.OK)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('61390bee-9d9a-4728-bcfc-439c112c061e')
+    @decorators.idempotent_id('61390bee-9d9a-4728-bcfc-439c112c061e')
     def test_delete_td(self):
         _, td = self.admin_client.create_td()
         resp, td = self.admin_client.delete_td(
@@ -68,7 +69,7 @@ class TestTransitDomains(base.BaseTest):
         self.assertEqual(resp.status, httplib.NO_CONTENT)
 
     @test.attr(type='smoke')
-    @test.idempotent_id('0f2cc7dc-049b-43fa-a7b0-41c044bcc09f')
+    @decorators.idempotent_id('0f2cc7dc-049b-43fa-a7b0-41c044bcc09f')
     def test_list_tds(self):
         _, td1 = self.admin_client.create_td()
         _, td2 = self.admin_client.create_td()
